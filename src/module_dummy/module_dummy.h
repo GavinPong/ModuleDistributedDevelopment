8a972a9b4d4d3648188647d8f6138dfcbab032a4
gavinpong <1910915035@qq.com>
2018-01-22 23:17:21 +0800

    1、调整主控Makefile和Makefile.param的位置 2、调整目录结构 3、供外部使用的数据结构用module_external文件集中管理

--- src/module_dummy/module_dummy.h
+++ src/module_dummy/module_dummy.h
@@ -5,6 +5,8 @@ Dummy is a sample file of how to use module
 #ifndef __MODULE_DUMMY_H__
 #define __MODULE_DUMMY_H__
 
+#include "module_base.h"
+
 
 
 
---
+++ src/module_external.h
@@ -0,0 +1,15 @@
+/******************************
+module_external.h is a file that storing the data structure used for common external use
+*******************************/
+#ifndef __MODULE_EXTERNAL_H__
+#define __MODULE_EXTERNAL_H__
+
+
+//Module common data structure
+typedef struct module_pub_data{
+
+}module_pub_data_t;
+
+
+
+#endif
